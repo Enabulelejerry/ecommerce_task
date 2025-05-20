ece2855944fe0b5ef0d6f3b3a17e4b17b0e05c0f
Enabulelejerry <jerryenabulele@gmail.com>
2025-05-20 12:47:18 +0100

    update

--- app/admin/Sidebar.tsx
+++ app/admin/Sidebar.tsx
@@ -12,8 +12,8 @@ function Sidebar() {
 		 {adminLinks.map((link)=>{
 			const isActivePage = pathname  === link.href
 			const variant = isActivePage? 'default':'ghost'
-			return <Button asChild className='w-full mb-2 capitalize font-normal justify-start' variant={variant}>
-				<Link key={link.href} href={link.href}>
+			return <Button key={link.href}  asChild className='w-full mb-2 capitalize font-normal justify-start' variant={variant}>
+				<Link href={link.href}>
 				   {link.label}
 				</Link>
 			</Button>
