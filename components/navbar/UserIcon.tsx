998ca07e0ac2ae6f35932e870a318f2f34cb6dc5
Enabulelejerry <jerryenabulele@gmail.com>
2025-07-23 12:14:58 +0100

    first commit

--- components/navbar/UserIcon.tsx
+++ components/navbar/UserIcon.tsx
@@ -1,16 +1,16 @@
-import { LuUser } from "react-icons/lu"
-import {currentUser,auth} from '@clerk/nextjs/server'
+import { LuUser } from "react-icons/lu";
+import { currentUser, auth } from "@clerk/nextjs/server";
 
 async function UserIcon() {
-  const user = await currentUser()
+  const user = await currentUser();
   const profileImage = user?.imageUrl;
 
-  if(profileImage){
-    return  (
-      <img src={profileImage} className='w-6 h-6 rounded-full object-cover' />
+  if (profileImage) {
+    return (
+      <img src={profileImage} className="w-6 h-6 rounded-full object-cover" />
     );
   }
-  return <LuUser className="w-6 h-6 bg-primary rounded-full text-white" />
+  return <LuUser className="w-6 h-6 bg-primary rounded-full text-white" />;
 }
 
-export default UserIcon
+export default UserIcon;
--- next.config.mjs
+++ next.config.mjs
@@ -8,7 +8,7 @@ const nextConfig = {
       },
       {
         protocol: 'https',
-        hostname: 'vodrpqziwcqasetysuai.supabase.co',
+        hostname: 'gngxvrxdmtymtvixtiwt.supabase.co',
       },
       {
         protocol: 'https',
